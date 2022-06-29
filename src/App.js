bc07c9d284e15f99064c41884e4db8372ff89289
my <my@gmail.com>
2022-06-29 18:33:24 -0400

    removed svg

--- src/App.js
+++ src/App.js
@@ -36,11 +36,10 @@ function App() {
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header> */}
-      <form onSubmit={handleSubmit}>
-      <div className="form-control w-full max-w-xs">
+      <form className="form-control w-full max-w-xs" onSubmit={handleSubmit}>
+      <div>
       {submitted && valid ? <div className="alert alert-success shadow-lg">
         <div>
-          <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
           <span>Your submission has been confirmed!</span>
         </div>
       </div> : null}
